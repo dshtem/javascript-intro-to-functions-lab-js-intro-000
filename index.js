8fd03537985f04b09749d76682fec7969838c237
dshtem@gmail.com <dshtem@gmail.com>
2018-04-03 21:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string) {
-  if(string.toUpperCase() === string) {
-    return "Yes INDEED!"
-  } else if (string ==== "I love you, Grandma") {
-    return "I love you too."
-  }
-  else {
-    return ("I can't hear you!")
-  }
-}
